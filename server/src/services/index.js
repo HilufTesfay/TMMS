164b0969922bd0f06474969f755606fa653cfa12
Hiluf Tesfay <hiluftesfay43@gmail.com>
2025-01-16 00:04:37 +0600

    refactor services/index to export bldService

--- server/src/services/index.js
+++ server/src/services/index.js
@@ -2,5 +2,6 @@ import userService from "./userService.js";
 import authService from "./authService.js";
 import tokenService from "./tokenService.js";
 import emailService from "./emailService.js";
+import bldService from "./bldService.js";
 
-export { userService, authService, tokenService, emailService };
+export { userService, authService, tokenService, emailService, bldService };
