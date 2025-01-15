d0846464be48c36642e62cb3d31b04702adf2603
Hiluf Tesfay <hiluftesfay43@gmail.com>
2025-01-16 02:46:30 +0600

    add classRoomService

---
+++ server/src/services/classRoomService.js
@@ -0,0 +1,34 @@
+import { ClassRoom } from "../models/index.js";
+import { CustomError } from "../utils/index.js";
+
+//define function to check if class roo is already exist
+const isRegistered = async (roomNumber) => {
+  const room = await ClassRoom.findOne({ roomNumber: roomNumber });
+  return !!room;
+};
+// define function to create class
+const addClassRoom = async (classRoomData) => {
+  const { roomNumber } = classRoomData;
+  if (await isRegistered(roomNumber)) {
+    throw new CustomError(400, `${roomNumber} is already exist`, true);
+  }
+  const classRoom = await ClassRoom.create(classRoomData);
+  if (!classRoom) {
+    throw new CustomError(400, "unable to create classrom", true);
+  }
+  return { message: `class room ${classRoom.roomNumber} added ` };
+};
+//define function to delete class room
+const deleteClassRoom = async (roomNumber) => {
+  const deletedClassRoom = await ClassRoom.deleteOne({
+    roomNumber: roomNumber,
+  });
+  if (deletedClassRoom.deletedCount === 0) {
+    throw new CustomError(400, "unable to delete building", true);
+  }
+  return {
+    message: `class room ${deleteClassRoom.roomNumber} deleted succcessfully`,
+  };
+};
+
+export default { addClassRoom, deleteClassRoom };
