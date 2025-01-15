b2c7059642ff632cd9ff28115fe36027db134e02
Hiluf Tesfay <hiluftesfay43@gmail.com>
2025-01-16 00:01:55 +0600

    add bldController.js

---
+++ server/src/controllers/bldController.js
@@ -0,0 +1,17 @@
+import { bldService } from "../services/index.js";
+import { handleCatchError } from "../utils/index.js";
+
+//define middleware to add buliding
+const addBuilding = handleCatchError(async (req, res) => {
+  const { message } = await bldService.addBuilding(req.body);
+  res.status(200).json({
+    messsage: message,
+  });
+});
+
+// define middleware to delete building
+const deleteBuilding = handleCatchError(async (req, res) => {
+  const { blockNumber } = req.body;
+  const { message } = await bldService.deleteBuilding(blockNumber);
+  res.status(202).json({ message: message });
+});
