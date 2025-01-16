c239ba2b4caf50b241134bcd7b2b63ddb97f1e40
Hiluf Tesfay <hiluftesfay43@gmail.com>
2025-01-16 12:48:34 +0600

    add feature isBuliding exist

--- server/src/services/classRoomService.js
+++ server/src/services/classRoomService.js
@@ -1,13 +1,22 @@
 import { ClassRoom } from "../models/index.js";
 import { CustomError } from "../utils/index.js";
-
+import { bldService } from "./index.js";
 //define function to check if class roo is already exist
 const isRegistered = async (roomNumber) => {
   const room = await ClassRoom.findOne({ roomNumber: roomNumber });
   return !!room;
 };
+//define function to check if buliding where the class is found if exist
+const isBuildingExist = async (blockNumber) => {
+  const bullding = await bldService.getBuilding(blockNumber);
+  return !!bullding;
+};
 // define function to create class
 const addClassRoom = async (classRoomData) => {
+  const { blockNumber } = classRoomData;
+  if (!isBuildingExist) {
+    throw new CustomError(400, `no block found with ${blockNumber}`);
+  }
   const { roomNumber } = classRoomData;
   if (await isRegistered(roomNumber)) {
     throw new CustomError(400, `${roomNumber} is already exist`, true);
