aba51bbfdc2fbf381373f765b1d59202ff465399
Hiluf Tesfay <hiluftesfay43@gmail.com>
2025-01-18 01:33:31 +0600

    add featute

--- server/src/controllers/classRoomController.js
+++ server/src/controllers/classRoomController.js
@@ -18,4 +18,37 @@ const deleteClassRoom = handleCatchError(async (req, res) => {
   });
 });
 
-export default { addClassRoom, deleteClassRoom };
+//get available class
+const getAvailableClassRooms = handleCatchError(async (req, res) => {
+  const classroms = await classRoomService.getAvailableClassRooms();
+  res.status(200).json(classroms);
+});
+
+//get taken class
+const getTakenClassRooms = handleCatchError(async () => {
+  const rooms = await classRoomService.getTakenClassRooms();
+  res.status(200).json(classroms);
+});
+
+//allocate class
+const allocateClassRoom = handleCatchError(async (req, res) => {
+  const { roomNumber } = req.body;
+  const { message } = await classRoomService.allocateClassRoom(roomNumber);
+  res.status(200).json(message);
+});
+
+// deallocate class
+const deallocateClassRoom = handleCatchError(async (req, res) => {
+  const { roomNumber } = req.body;
+  const { message } = await classRoomService.deallocateClassRoom(roomNumber);
+  res.status(200).json(message);
+});
+
+export default {
+  addClassRoom,
+  deleteClassRoom,
+  getAvailableClassRooms,
+  getTakenClassRooms,
+  deallocateClassRoom,
+  allocateClassRoom,
+};
